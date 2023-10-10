d50a1297b759ba3c4ffdd0ca281626f660dd479a
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2023-10-10 21:48:18 +0700

    v0.5
    Added Block Mechanic

--- javascript.js
+++ javascript.js
@@ -1,21 +1,45 @@
 let enemyHP = 50
 let playerHP = 50
+let diceDisplay = document.querySelector("#dice");
+
 
 function attack(){
-    let attackRandom = Math.floor(Math.random() * 6 + 1)
-    enemyHP -= attackRandom;
+    let playerAttackRandom = Math.floor(Math.random() * 6 + 1)
+    enemyHP -= playerAttackRandom;
 
-    document.querySelector("#dice").setAttribute("src", "dice/dice" + attackRandom + ".svg"); 
-
+    diceDisplay.setAttribute("src", "dice/dice" + playerAttackRandom + ".svg");
     enemyHealth.style.width = enemyHP*2 + "%";
-    if(enemyHP<0){
-        enemyHealth.style.width = "0%";
-        gameOver()
+    
+    if(enemyHP<1){
+        gameOver();
+    }
+
+    enemyTurn();
+}
+
+function enemyTurn(a){
+    let enemyAttackRandom = Math.floor(Math.random() * 6 + 1)
+
+    if(a==="block"){
+        enemyAttackRandom /= 4;
+    }
+
+    playerHP -= enemyAttackRandom;
+    playerHealth.style.width = playerHP*2 + "%";
+
+    if(playerHP<1){
+        gameOver();
     }
 }
 
+function block(){
+    enemyTurn("block");
+}
+
 function gameOver(){
-    alert("Game Over!")
+    alert("Game Over!");
     enemyHP=50;
+    playerHP=50;
     enemyHealth.style.width = "100%";
+    playerHealth.style.width = "100%";
 }
