beb67d0ba0b3bc32fb08264339265d71c83bf323
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2023-10-10 20:43:20 +0700

    v0.4
    implemented attack and HP

--- javascript.js
+++ javascript.js
@@ -1,5 +1,21 @@
-attackButton.onclick=function(){
-    var randnum = Math.floor(Math.random() * 6 + 1)
+let enemyHP = 50
+let playerHP = 50
 
-    document.querySelector("#dice").setAttribute("src", "dice/dice" + randnum + ".svg"); 
+function attack(){
+    let attackRandom = Math.floor(Math.random() * 6 + 1)
+    enemyHP -= attackRandom;
+
+    document.querySelector("#dice").setAttribute("src", "dice/dice" + attackRandom + ".svg"); 
+
+    enemyHealth.style.width = enemyHP*2 + "%";
+    if(enemyHP<0){
+        enemyHealth.style.width = "0%";
+        gameOver()
+    }
+}
+
+function gameOver(){
+    alert("Game Over!")
+    enemyHP=50;
+    enemyHealth.style.width = "100%";
 }
