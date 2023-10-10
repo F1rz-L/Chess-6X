c0cdcd55e16cc8a1b5f7bad4fe665bd7795c7075
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2023-10-10 19:01:00 +0700

    v0.3
    refined gui

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-diceroll.onclick=function(){
+attackButton.onclick=function(){
     var randnum = Math.floor(Math.random() * 6 + 1)
 
     document.querySelector("#dice").setAttribute("src", "dice/dice" + randnum + ".svg"); 
